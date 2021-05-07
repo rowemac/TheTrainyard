762c174ab27aa025e43607d97e2bb4f1c1135072
Mac Rowe <m.rowe42@gmail.com>
2021-05-07 14:46:49 -0400

    Update create_users migration

--- db/migrate/20210507163431_create_users.rb
+++ db/migrate/20210507163431_create_users.rb
@@ -5,8 +5,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :username
       t.string :email
       t.string :password_digest
-      t.string :uid
-      t.string :provider
 
       t.timestamps
     end
