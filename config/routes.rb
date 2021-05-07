8c6e4c72d48da24fe8ef741cd1402f50b305ee27
Mac Rowe <m.rowe42@gmail.com>
2021-05-07 14:04:44 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do 
+    resources :tickets
+  end 
+
+  resources :concerts, only: [:index, :show]
 end
