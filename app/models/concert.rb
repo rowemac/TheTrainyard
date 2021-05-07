7ce3f0831798791afd98637edff98f69e36482a8
Mac Rowe <m.rowe42@gmail.com>
2021-05-07 12:51:21 -0400

    Add has_many macros to user and concert models

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,2 +1,4 @@
 class Concert < ApplicationRecord
+    has_many :tickets
+    has_many :users, through: :tickets
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :tickets
+    has_many :concerts, through: :tickets
 end
