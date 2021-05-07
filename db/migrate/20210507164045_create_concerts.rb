6972e046c9614e096429fc6e2a6c1d166a63f33f
Mac Rowe <m.rowe42@gmail.com>
2021-05-07 14:48:57 -0400

    Update create_concerts migration

--- db/migrate/20210507164045_create_concerts.rb
+++ db/migrate/20210507164045_create_concerts.rb
@@ -1,11 +1,13 @@
 class CreateConcerts < ActiveRecord::Migration[6.1]
   def change
     create_table :concerts do |t|
-      t.datetime :date
+      t.date :date
+      t.time :time
       t.string :title
       t.string :headliner
       t.string :opener
-      t.boolean :sold_out
+      t.boolean :sold_out, default: false
+      t.float :price
 
       t.timestamps
     end
