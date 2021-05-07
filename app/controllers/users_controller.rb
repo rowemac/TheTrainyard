39879345de9e7b9910010bb367536a1485131d09
Mac Rowe <m.rowe42@gmail.com>
2021-05-07 16:04:27 -0400

    Add empty methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
 
+    def new
+
+    end 
+
+    def create
+
+    end 
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
     def show
         @user = User.find_by_id(params[:id])
+    end 
+
+    def destroy
+
     end 
 
 end
