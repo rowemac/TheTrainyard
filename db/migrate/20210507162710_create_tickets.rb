7b4aa133df195c3bb29bee70b74d4c105f15047f
Mac Rowe <m.rowe42@gmail.com>
2021-05-07 14:45:47 -0400

    Update create_tickets migration

--- db/migrate/20210507162710_create_tickets.rb
+++ db/migrate/20210507162710_create_tickets.rb
@@ -3,7 +3,6 @@ class CreateTickets < ActiveRecord::Migration[6.1]
     create_table :tickets do |t|
       t.integer :confirmation
       t.integer :quantity, default: 0
-      t.float :price
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :concert, null: false, foreign_key: true
 
