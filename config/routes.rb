9f2a0cbf4c64c05a3d47b942760e66bfe93992dd
Mac Rowe <m.rowe42@gmail.com>
2021-05-07 14:12:48 -0400

    Update routes to not have a users index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users do 
+  resources :users, only: [:show, :new, :create, :edit, :update, :delete] do 
     resources :tickets
   end 
 
