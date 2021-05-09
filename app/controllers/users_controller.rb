486213c8eb647ec888a94830ea3c2ad14b17e5ab
Mac Rowe <m.rowe42@gmail.com>
2021-05-09 12:19:33 -0400

    Add code to new and create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
 
-    def new
+    before_action :find_user
 
+    def new
+        @user = User.new
     end 
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end 
     end 
 
     def edit
@@ -17,11 +25,21 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find_by_id(params[:id])
+        
     end 
 
     def destroy
 
-    end 
+    end
 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :username, :email, :password)
+    end
+
+    def find_user
+        @user = User.find_by_id(params[:id])
+    end 
 end
