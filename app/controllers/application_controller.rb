a7fc960f5c6f20df07cee7d64e2df90535e65842
Mac Rowe <m.rowe42@gmail.com>
2021-05-09 13:08:53 -0400

    Add current_user, logged_in?, and error_page methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def current_user
+        @user ||= User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+
+    def error_page
+        render '/error_page'
+    end
+
 end
