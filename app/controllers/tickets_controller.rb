33f04003e6c6b2b82d2741e76dae6ef7c352ab0a
Mac Rowe <m.rowe42@gmail.com>
2021-05-10 11:53:37 -0400

    Add ticket iundex method

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,37 @@
 class TicketsController < ApplicationController
+    
+    # before_action :find_ticket
+
+    def index
+        @tickets = Ticket.all
+    end 
+
+    def new
+    end 
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end 
 
     def show
+    end
 
+    def destroy
+    end
+
+    private
+
+    def ticket_params
+        params.require(:ticket).permit(:confirmation, :quantity)
+    end
+
+    def find_ticket
+        @ticket = Ticket.find_by_id(params[:id])
     end
 
 end
