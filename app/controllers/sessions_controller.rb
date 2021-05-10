28433d434a21f3a1acb1d5fd865b963581675f0e
Mac Rowe <m.rowe42@gmail.com>
2021-05-10 08:45:47 -0400

    Update redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to concerts_path
+    else
+      redirect_to '/login'
+    end 
   end
 
   def destroy
+    session.clear
+    redirect_to '/login'
   end
 end
