519f0c35f14394a2e024d53ed8116d00f876c0ae
Mac Rowe <m.rowe42@gmail.com>
2021-05-11 14:29:35 -0400

    Update routes for tickets to not include index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update, :show, :delete]
 
   resources :concerts, only: [:index, :show] do
-    resources :tickets, only: [:new, :create, :edit, :update, :show, :delete]
+    resources :tickets, only: [:show, :new, :create, :edit, :update, :destroy]
   end 
 
   get 'login', to: 'sessions#new'
