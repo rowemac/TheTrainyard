147e7eb5dae7ffb2eb62565ded5b74883e63fdfa
Mac Rowe <m.rowe42@gmail.com>
2021-05-11 14:26:17 -0400

    Update validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :tickets, dependent: :destroy
     has_many :concerts, through: :tickets
-    validates_presence_of :name, :username, :email
+    validates :name, :username, :email, presence: true
+    validates :username, :email, uniqueness: true 
 end
