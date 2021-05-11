2ef15df6e3d5c483d86ac8c83d6d809e2c6771da
Mac Rowe <m.rowe42@gmail.com>
2021-05-11 09:34:10 -0400

    Update current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    helper_method :logged_in?
-    helper_method :current_user
-    helper_method :error_page 
+    helper_method :logged_in?, :current_user, :error_page
 
     def current_user
-        @user ||= User.find_by_id(session[:user_id])
+        @user = User.find_by_id(session[:user_id])
     end
 
     def logged_in?
