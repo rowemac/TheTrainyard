706e617f459c98b41c94b6f8bf8900b9a5f815dd
Mac Rowe <m.rowe42@gmail.com>
2021-05-11 14:25:22 -0400

    Remove unused code in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
     before_action :find_user, only: [:show, :edit, :update, :destroy]
+    before_action :logged_in?, only: [:show, :edit, :update, :destroy]
 
     def new
         @user = User.new
