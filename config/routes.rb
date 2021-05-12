85bbf4ccadde3317416cab9abaeeb39572e56137
Mac Rowe <m.rowe42@gmail.com>
2021-05-12 12:27:34 -0400

    Update routes to reflect new home page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/', to: "application#home"
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   get '/:anything', to: 'application#wrong_page'
 
