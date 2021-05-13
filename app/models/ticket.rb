35a11dfaf7fecfc28dfb26615e96d45b9ce9df6c
Mac Rowe <m.rowe42@gmail.com>
2021-05-13 11:10:44 -0400

    Update validation for ticket to not be 0 or negative number

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :concert
-  validates :quantity, presence: true  
+  # validates :quantity, presence: true
+  validates :quantity, :numericality => { :greater_than => 0 } 
 end
