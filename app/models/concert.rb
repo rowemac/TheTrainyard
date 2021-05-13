f9a93531df517a9faf880aca1e310f3db9a064c3
Mac Rowe <m.rowe42@gmail.com>
2021-05-13 08:13:12 -0400

    Remove commented-out code

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -2,6 +2,5 @@ class Concert < ApplicationRecord
     has_many :tickets
     has_many :users, through: :tickets
 
-    # scope :purchasable, -> { where(sold_out: false ) }
     scope :ordered_by_date, -> { order(date: :asc) }
 end
