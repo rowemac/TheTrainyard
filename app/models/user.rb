c9768f2f3e11db8391e0844d98cadba75249a305
Mac Rowe <m.rowe42@gmail.com>
2021-05-13 11:28:15 -0400

    Add dependent destroy to User concerts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :tickets, dependent: :destroy
-    has_many :concerts, through: :tickets
+    has_many :concerts, through: :tickets, dependent: :destroy
     validates :name, :username, :email, presence: true
     validates :username, :email, uniqueness: true 
 end
