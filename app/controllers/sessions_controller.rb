6aeb991802ef02ca79e485cddd35ade7f9385db8
Mac Rowe <m.rowe42@gmail.com>
2021-05-13 20:57:47 -0400

    Add omniauth methods for google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to concerts_path
@@ -14,6 +14,26 @@ class SessionsController < ApplicationController
       flash[:notice] = "Username and/or Password are invalid. Please try again."
       redirect_to '/login'
     end 
+  end
+
+
+  def google
+    @user = User.find_or_create_by(email: auth["info"]["email"]) do |user| 
+        user.password =  SecureRandom.hex(10)
+        user.name = auth["info"]["name"]
+        user.username = auth["info"]["name"]
+    end 
+    if @user && @user.id
+        session[:user_id] = @user.id
+        redirect_to user_path(@user) 
+    else
+      flash[:notice] = "Something went wrong."
+        redirect_to "/"
+    end 
+  end
+
+  def auth
+    request.env['omniauth.auth']
   end
 
   def destroy
