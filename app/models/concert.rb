f5ae1c527be7d8e3ba38873a4475059d697b4e06
Mac Rowe <m.rowe42@gmail.com>
2021-09-01 14:59:18 -0400

    Comment out coding challenge code

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -5,4 +5,5 @@ class Concert < ApplicationRecord
     scope :ordered_by_date, -> { order(date: :asc) }
     scope :early_concerts, -> { where(time: "19:00:00").order(date: :asc) }
     # scope :search_headliner, -> (headliner) { where("headliner like ?", headliner) }
+    # scope :popular, -> { joins(:tickets).group("tickets.id").order("count(tickets.id)").distinct.limit(5) } 
 end
